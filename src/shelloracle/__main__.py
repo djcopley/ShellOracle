4bf6d5065db78bb825c3088a20793ccd5258d946
Daniel <djcopley@users.noreply.github.com>
2024-01-04 12:50:57 -0500

    Update installation process (#26)

--- src/shelloracle/__main__.py
+++ src/shelloracle/__main__.py
@@ -1,4 +1,24 @@
+import argparse
+
 from . import shelloracle
+from .bootstrap import bootstrap
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--init", help="initialize shelloracle with scripts",
+                        action="store_true")
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    if args.init:
+        bootstrap()
+        exit(0)
+
+    shelloracle.cli()
+
 
 if __name__ == "__main__":
     shelloracle.cli()
--- src/shelloracle/bootstrap.py
+++ src/shelloracle/bootstrap.py
@@ -1,7 +1,4 @@
 import os
-import shutil
-import subprocess
-import zipfile
 from pathlib import Path
 
 from prompt_toolkit import print_formatted_text
@@ -18,59 +15,36 @@ def print_error(error: str) -> None:
     print_formatted_text(FormattedText([("ansired", error)]))
 
 
-def pip_output(text: str) -> None:
-    print_formatted_text(FormattedText([("ansigray", text)]), end="")
-
-
 def replace_home_with_tilde(path: Path) -> Path:
     relative_path = path.relative_to(Path.home())
     return Path("~") / relative_path
 
 
-def ensure_zsh():
+def ensure_zsh() -> None:
     shell = os.environ.get("SHELL")
+    if shell is None:
+        print_error("Unable to determine shell environment. If you are confident "
+                    "that you are running in zsh, run again with `SHELL=zsh python3 -m shelloracle --init`")
+        exit(1)
     if "zsh" not in shell:
         print_error(f"'{shell}' is currently unsupported. "
                     f"Please open an issue https://github.com/djcopley/ShellOracle/issues.")
         exit(1)
 
 
-def install_shelloracle():
-    print_info("Installing shelloracle")
-    python = shutil.which("python3")
-    pip = subprocess.Popen([python, "-m", "pip", "install", "--upgrade", "shelloracle"],
-                           stdout=subprocess.PIPE, text=True)
-    for line in pip.stdout.readlines():
-        pip_output(line)
-    if (ret := pip.wait()) == 0:
-        print_info("Successfully installed shelloracle")
-    else:
-        print_error(f"Unable to install shelloracle")
-        exit(ret)
-
-
 zshrc_path = Path.home() / ".zshrc"
 shelloracle_zsh_dest = Path.home() / ".shelloracle.zsh"
 
 
-def read_shelloracle_zsh():
-    working_dir = Path(__file__).parent.absolute()
-    zsh_path = "shelloracle.zsh"
-    if working_dir.suffix == ".pyz":
-        with zipfile.ZipFile(working_dir, "r") as zip_app:
-            shelloracle_zsh = zip_app.read(zsh_path)
-    else:
-        shelloracle_zsh = (working_dir / zsh_path).read_bytes()
-    return shelloracle_zsh
-
-
-def write_shelloracle_zsh():
-    shelloracle_zsh = read_shelloracle_zsh()
+def write_shelloracle_zsh() -> None:
+    zsh_path = Path(__file__).parent.absolute() / "shelloracle.zsh"
+    shelloracle_zsh = zsh_path.read_bytes()
     shelloracle_zsh_dest.write_bytes(shelloracle_zsh)
     print_info(f"Successfully wrote key bindings to {replace_home_with_tilde(shelloracle_zsh_dest)}")
 
 
-def update_zshrc():
+def update_zshrc() -> None:
+    zshrc_path.touch(exist_ok=True)
     with zshrc_path.open("r") as file:
         zshrc = file.read()
     line = f"[ -f {shelloracle_zsh_dest} ] && source {shelloracle_zsh_dest}"
@@ -81,17 +55,16 @@ def update_zshrc():
     print_info(f"Successfully updated {replace_home_with_tilde(zshrc_path)}")
 
 
-def install():
+def bootstrap() -> None:
     with create_app_session_from_tty():
         ensure_zsh()
-        install_shelloracle()
 
         if confirm("Enable terminal keybindings?", suffix=" ([y]/n) ") is False:
-            exit(0)
+            return
 
         write_shelloracle_zsh()
         update_zshrc()
 
 
 if __name__ == '__main__':
-    install()
+    bootstrap()
