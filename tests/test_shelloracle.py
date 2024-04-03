7b305a84a0b1e6c2422ed5819d9f55919afa2ab3
Daniel Copley <djcopley@users.noreply.github.com>
2024-04-02 20:11:53 -0400

    Add spinner test

--- tests/test_shelloracle.py
+++ tests/test_shelloracle.py
@@ -1,9 +1,29 @@
 import os
 import sys
+from unittest.mock import MagicMock, call
 
 import pytest
+from yaspin.spinners import Spinners
 
-from shelloracle.shelloracle import get_query_from_pipe
+from shelloracle.config import Configuration
+from shelloracle.shelloracle import get_query_from_pipe, spinner
+
+
+def test_spinner(monkeypatch):
+    yaspin_mock = MagicMock()
+    monkeypatch.setattr("shelloracle.shelloracle.yaspin", yaspin_mock)
+
+    monkeypatch.setattr(Configuration, "spinner_style", None)
+    spinner()
+    assert yaspin_mock.call_args == call()
+
+    monkeypatch.setattr(Configuration, "spinner_style", "earth")
+    spinner()
+    assert yaspin_mock.call_args == call(Spinners.earth)
+
+    monkeypatch.setattr(Configuration, "spinner_style", "not a real spinner")
+    with pytest.raises(AttributeError):
+        spinner()
 
 
 def test_get_query_from_pipe(monkeypatch):
